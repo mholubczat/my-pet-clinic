a4d117b3a14aa7f4193de52a5e5bdf84412408b9
m.holubczat <m.holubczat@gmail.com>
2021-03-06 14:13:44 +0100

    add base entity. Closes #10.

--- pet-clinic-data/src/main/java/me/mholubczat/mypetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/me/mholubczat/mypetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package me.mholubczat.mypetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/me/mholubczat/mypetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/me/mholubczat/mypetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package me.mholubczat.mypetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
     private String name;
 
     public String getName() {
