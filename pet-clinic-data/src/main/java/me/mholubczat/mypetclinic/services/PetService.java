81d5bdbf7ee886f4b24539fd3070941d9ee8cc7b
m.holubczat <m.holubczat@gmail.com>
2021-03-06 14:03:37 +0100

    create interfaces for services. Closes #5.

---
+++ pet-clinic-data/src/main/java/me/mholubczat/mypetclinic/services/PetService.java
@@ -0,0 +1,15 @@
+package me.mholubczat.mypetclinic.services;
+
+
+import me.mholubczat.mypetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/me/mholubczat/mypetclinic/services/VetService.java
@@ -0,0 +1,15 @@
+package me.mholubczat.mypetclinic.services;
+
+
+
+import me.mholubczat.mypetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findByLastName(String lastName);
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
