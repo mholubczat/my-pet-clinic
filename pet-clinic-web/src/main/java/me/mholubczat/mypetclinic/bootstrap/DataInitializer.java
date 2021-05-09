50cf58f0fd292f75e2e657b308af42336b63960d
m.holubczat <m.holubczat@gmail.com>
2021-05-09 21:09:46 +0200

    add PetTypes to DataLoader. closes #32

--- pet-clinic-web/src/main/java/me/mholubczat/mypetclinic/bootstrap/DataInitializer.java
+++ pet-clinic-web/src/main/java/me/mholubczat/mypetclinic/bootstrap/DataInitializer.java
@@ -1,11 +1,11 @@
 package me.mholubczat.mypetclinic.bootstrap;
 
 import me.mholubczat.mypetclinic.model.Owner;
+import me.mholubczat.mypetclinic.model.PetType;
 import me.mholubczat.mypetclinic.model.Vet;
 import me.mholubczat.mypetclinic.services.OwnerService;
+import me.mholubczat.mypetclinic.services.PetTypeService;
 import me.mholubczat.mypetclinic.services.VetService;
-import me.mholubczat.mypetclinic.services.map.OwnerServiceMap;
-import me.mholubczat.mypetclinic.services.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -14,14 +14,26 @@ public class DataInitializer implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataInitializer(OwnerService ownerService, VetService vetService) {
+    public DataInitializer(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
+        System.out.println("Loaded pet types....");
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Barbara");
